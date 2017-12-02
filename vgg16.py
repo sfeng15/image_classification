5e7bb746b2f084308f443088264fa2564efc0c93
DESKTOP-QQ860R3\shuof <shuofeng6@gmail.com>
2017-12-02 17:53:39 -0600

    add batch normalization

--- vgg16.py
+++ vgg16.py
@@ -96,6 +96,37 @@ class Vgg16(object):
 
     def max_pool(self, input_op, name, kh, kw, dh, dw):
         return tf.nn.max_pool(input_op, ksize=[1, kh, kw, 1], strides=[1, dh, dw, 1], padding='SAME', name=name)
+    
+    def batch_norm(self, x, n_out):
+        """
+        Batch normalization on convolutional maps.
+        Ref.: http://stackoverflow.com/questions/33949786/how-could-i-use-batch-normalization-in-tensorflow
+        Args:
+            x:           Tensor, 4D BHWD input maps
+            n_out:       integer, depth of input maps
+            phase_train: boolean tf.Varialbe, true indicates training phase
+            scope:       string, variable scope
+        Return:
+            normed:      batch-normalized maps
+        """
+        with tf.variable_scope('bn'):
+            beta = tf.Variable(tf.constant(0.0, shape=[n_out]),
+                                         name='beta', trainable=True)
+            gamma = tf.Variable(tf.constant(1.0, shape=[n_out]),
+                                          name='gamma', trainable=True)
+            batch_mean, batch_var = tf.nn.moments(x, [0,1,2], name='moments')
+            ema = tf.train.ExponentialMovingAverage(decay=0.5)
+
+            def mean_var_with_update():
+                ema_apply_op = ema.apply([batch_mean, batch_var])
+                with tf.control_dependencies([ema_apply_op]):
+                    return tf.identity(batch_mean), tf.identity(batch_var)
+
+            mean, var = tf.cond(self.phase_train,
+                                mean_var_with_update,
+                                lambda: (ema.average(batch_mean), ema.average(batch_var)))
+            normed = tf.nn.batch_normalization(x, mean, var, beta, gamma, 1e-3)
+        return normed
 
     def conv_layer(self, input_op, name, kh, kw, n_out, dh, dw):
         n_in = input_op.get_shape()[-1].value
@@ -107,7 +138,8 @@ class Vgg16(object):
                                 n_out], dtype=tf.float32, initializer=tf.constant_initializer())
             conv = tf.nn.conv2d(input_op, W, strides=[
                                 1, dh, dw, 1], padding='SAME') + B
-            activation = tf.nn.relu(conv)
+            BN = self.batch_norm(conv, n_out)
+            activation = tf.nn.relu(BN)
 
         return activation
 
@@ -137,3 +169,4 @@ class Vgg16(object):
     def update_op(self, loss, learning_rate):
         optimizer = tf.train.AdamOptimizer(learning_rate).minimize(loss)
         return optimizer
+
